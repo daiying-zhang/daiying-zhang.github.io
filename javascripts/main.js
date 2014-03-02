44cedb7fa754617074fc1f27646abc8ff8b1509f
daiying.zhang <daiying.zhang@qunar.com>
2014-03-02 19:21:19 +0800

    update style

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,14 +2,12 @@
     function $(id){
         return document.getElementById(id)
     }
-
     var trigger = $('js-menu-icon');
     trigger && (trigger.onclick = function(e){
         var nav = $('js-nav'),
             statu = nav.style.display;
-
         if(statu === 'none'){
-            nav.style.display = ''
+            nav.style.display = 'block'
         }else{
             nav.style.display = 'none'
         }
