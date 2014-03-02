3463a9c7e89310af8cc87cb02df3a5ce315b0190
daiying.zhang <daiying.zhang@qunar.com>
2014-03-02 19:08:55 +0800

    update main

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+(function(){
+    function $(id){
+        return document.getElementById(id)
+    }
+
+    var trigger = $('js-menu-icon');
+    console.log(trigger);
+    trigger && (trigger.onclick = function(e){
+        console.log('ddd');
+        var nav = $('js-nav'),
+            statu = nav.style.display;
+
+        if(statu === 'none'){
+            nav.style.display = ''
+        }else{
+            nav.style.display = 'none'
+        }
+    })
+});
